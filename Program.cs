537461a1f0995bbab5eecfe5ca2ef50039fa2af9
Zagidullin-Bulat <120476991+Zagidullin-Bulat@users.noreply.github.com>
2022-12-29 18:13:07 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых на плоскости, заданных уравнениями 
+
+Console.WriteLine ("Введите k1:");
+double k1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine ("Введите k2:");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine ("Введите b1:");
+double b1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine ("Введите b2:");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+double x = 0;
+double y = 0;
+
+if ((k1 == k2) && (b1 == b2))
+    {
+        Console.WriteLine ("Прямые совпадают");
+    }
+else if (k1==k2)
+    {
+        Console.WriteLine ("Прямые параллельны");
+    }
+
+else
+    {
+    x=(b2-b1)/(k1-k2);
+    y=(k1*(b2-b1))/(k1-k2)+b1;
+    Console.WriteLine ($"Переменная x={x}, переменная y={y}");
+    }
