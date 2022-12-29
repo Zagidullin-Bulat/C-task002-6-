cc370639ee4356d2f81a51fe513401e436083f8f
Zagidullin-Bulat <120476991+Zagidullin-Bulat@users.noreply.github.com>
2022-12-29 18:10:32 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь. Не использовать массив.
+
+Console.WriteLine("Введите колличество элементов: ");
+int M = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+for (int numbers=0; M>numbers; numbers++)
+    {
+        Console.WriteLine ("Введите число:");
+        int number = Convert.ToInt32(Console.ReadLine());
+        if (number > 0)
+        {
+            count++;
+        }
+    }
+
+Console.WriteLine($"Кол-во элементов {M}, больше 0 равно {count}");
